beac7a8edcd0e126e00099158f90f8b59fcb69e8
Jehanno <manuel.jehanno.etu@univ-lemans.fr>
2020-11-13 16:46:22 +0100

    fin TP1
    avancement homepage

--- app/app/src/main/java/com/example/starsnow/Splashscreen.java
+++ app/app/src/main/java/com/example/starsnow/Splashscreen.java
@@ -2,7 +2,12 @@ package com.example.starsnow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Splashscreen extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class Splashscreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splashscreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
     }
 }
