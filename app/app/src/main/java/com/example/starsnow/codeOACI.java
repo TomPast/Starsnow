639edc30c03d6e7ab1b5fcb1d77312954c5418e2
Tom <tom.pastor.etu@univ-lemans.fr>
2020-11-13 12:01:07 +0100

    CodeOACI activity

---
+++ app/app/src/main/java/com/example/starsnow/codeOACI.java
@@ -0,0 +1,14 @@
+package com.example.starsnow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class codeOACI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_code_o_a_c_i);
+    }
+}
