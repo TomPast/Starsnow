a6b46eac1604f3c5802cf26b895b548ca1606716
Tom <tom.pastor.etu@univ-lemans.fr>
2020-11-13 16:38:09 +0100

    Page codé décode + Classes du modèles

---
+++ app/app/src/main/java/Aeroport.java
@@ -0,0 +1,51 @@
+public class Aeroport {
+    private String OACI;
+    private String nom;
+    private Snowtam snowtam;
+    private double latitude;
+    private double longitude;
+
+    public Aeroport(String OACI) {
+        this.setOACI(OACI);
+    }
+
+    public String getOACI() {
+        return OACI;
+    }
+
+    public void setOACI(String OACI) {
+        this.OACI = OACI;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Snowtam getSnowtam() {
+        return snowtam;
+    }
+
+    public void setSnowtam(Snowtam snowtam) {
+        this.snowtam = snowtam;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
---
+++ app/app/src/main/java/Snowtam.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+
+public class Snowtam {
+    private String OACI;
+    private String plainCodedSnowtam;
+    private HashMap<String, String> DecodedSnowtam = new HashMap<String, String>();
+
+    public Snowtam(String codeOACI){
+        this.setOACI(OACI);
+    }
+
+    public Snowtam(String codeOACI, String plainCodedSnowtam){
+        this.setOACI(OACI);
+    }
+
+    public String getOACI() {
+        return OACI;
+    }
+
+    public void setOACI(String OACI) {
+        this.OACI = OACI;
+    }
+
+    public String getPlainCodedSnowtam() {
+        return plainCodedSnowtam;
+    }
+
+    public void setPlainCodedSnowtam(String plainCodedSnowtam) {
+        this.plainCodedSnowtam = plainCodedSnowtam;
+    }
+
+    public HashMap<String, String> getDecodedSnowtam() {
+        return DecodedSnowtam;
+    }
+
+    public void setDecodedSnowtam(HashMap<String, String> decodedSnowtam) {
+        DecodedSnowtam = decodedSnowtam;
+    }
+}
