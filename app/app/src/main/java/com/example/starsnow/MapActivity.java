176217861aaf3e48fc10e20dfc9e6f10b5904b3b
Maxime <garsbiz72@gmail.com>
2020-11-13 16:47:58 +0100

    Map fonctionnelle en dur

--- app/app/src/main/java/com/example/starsnow/MapActivity.java
+++ app/app/src/main/java/com/example/starsnow/MapActivity.java
@@ -3,12 +3,19 @@ package com.example.starsnow;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 public class MapActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
+        private MapFragment myMapFragment;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_map);
+
+            FragmentManager fragmentManager = this.getSupportFragmentManager();
+            this.myMapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.fragment_map);
+
+        }
     }
-}
---
+++ app/app/src/main/java/com/example/starsnow/MapFragment.java
@@ -0,0 +1,44 @@
+package com.example.starsnow;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class MapFragment extends SupportMapFragment implements OnMapReadyCallback {
+
+    private GoogleMap googleMap;
+
+    public MapFragment()  {
+        getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(final GoogleMap gmap) {
+        this.googleMap = gmap;
+
+        // Set default position
+        // Add a marker in Sydney and move the camera
+        LatLng NewYorkAirport = new LatLng(40.6413111, -73.7781391);
+        this.googleMap.addMarker(new MarkerOptions().position(NewYorkAirport).title("Aéroport JFK"));
+        this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(NewYorkAirport));
+
+        /*this.googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(latLng);
+                markerOptions.title(latLng.latitude + " : "+ latLng.longitude);
+                // Clear previously click position.
+                googleMap.clear();
+                // Zoom the Marker
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                // Add Marker on Map
+                googleMap.addMarker(markerOptions);
+            }
+        });*/
+    }
+}
