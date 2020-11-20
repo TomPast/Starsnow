357defc866e89bb75abb557f3e5bddca4f01a978
Maxime <garsbiz72@gmail.com>
2020-11-20 16:37:24 +0100

    Map correcte avec possibilité de changer de vue

--- app/app/src/main/java/com/example/starsnow/MapActivity.java
+++ app/app/src/main/java/com/example/starsnow/MapActivity.java
@@ -1,21 +1,63 @@
 package com.example.starsnow;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentActivity;
 
-public class MapActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-        private MapFragment myMapFragment;
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
-        @Override
+    //private MapFragment myMapFragment;
+    private GoogleMap googleMap;
+
+    @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_map);
 
-            FragmentManager fragmentManager = this.getSupportFragmentManager();
-            this.myMapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.fragment_map);
+        Button button = findViewById(R.id.imageButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+               if(googleMap.getMapType() == GoogleMap.MAP_TYPE_HYBRID){
+                   googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                   button.setBackground(getDrawable(R.drawable.satellite));
+               }
+               else{
+                   googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                   button.setBackground(getDrawable(R.drawable.norm));
+               }
+            }
+        });
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_map);
+        mapFragment.getMapAsync(this);
 
         }
+
+
+    public void backFunction(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
+
+    @Override
+    public void onMapReady(GoogleMap gMap) {
+        this.googleMap = gMap;
+
+        // Set default position
+        // Add a marker in Sydney and move the camera
+        LatLng NewYorkAirport = new LatLng(40.6413111, -73.7781391);
+        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        this.googleMap.addMarker(new MarkerOptions().position(NewYorkAirport).title("Aéroport JFK"));
+        this.googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(NewYorkAirport,14));
+    }
+}
