92f7b561cbb37c6bf579b15209fad7864b5724c8
Jehanno <manuel.jehanno.etu@univ-lemans.fr>
2020-11-20 15:53:18 +0100

    Cards fonctionnelles

--- app/app/src/main/java/com/example/starsnow/MainActivity.java
+++ app/app/src/main/java/com/example/starsnow/MainActivity.java
@@ -1,26 +1,62 @@
 package com.example.starsnow;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private ListView liste_aeroports;
-    private String[] aeroports = new String[]{
-            "New York", "Paris", "Dubai", "Londres"
-    };
+    private CardView cardAerop1, cardAerop2, cardAerop3, cardAerop4;
+    private Button ajoutAerop;
+    private int nbAerop = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-/*
-        liste_aeroports = (ListView) findViewById(R.id.liste_aeroports);
 
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
-                android.R.layout.simple_list_item_1, aeroports);
-        liste_aeroports.setAdapter(adapter); */
+        cardAerop1 = (CardView) findViewById(R.id.card_aerop1);
+        cardAerop2 = (CardView) findViewById(R.id.card_aerop2);
+        cardAerop3 = (CardView) findViewById(R.id.card_aerop3);
+        cardAerop4 = (CardView) findViewById(R.id.card_aerop4);
+
+        cardAerop1.setVisibility(View.GONE);
+        cardAerop2.setVisibility(View.GONE);
+        cardAerop3.setVisibility(View.GONE);
+        cardAerop4.setVisibility(View.GONE);
+
+        ajoutAerop = (Button) findViewById(R.id.ajoutAerop);
+
+        ajoutAerop.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(nbAerop == 0)
+        {
+            cardAerop1.setVisibility(View.VISIBLE);
+            nbAerop++;
+        }
+        else if (nbAerop == 1) {
+            cardAerop2.setVisibility(View.VISIBLE);
+            nbAerop++;
+        }
+        else if (nbAerop == 2) {
+            cardAerop3.setVisibility(View.VISIBLE);
+            nbAerop++;
+        }
+        else if (nbAerop == 3) {
+            cardAerop4.setVisibility(View.VISIBLE);
+            ajoutAerop.setVisibility(View.GONE);
+            nbAerop = 4;
+        }
+        else {
+            //affiche popup 
+        }
     }
 }
