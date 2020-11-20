755deb4f666d5b6857281ae6315b971a14d3abb9
Jehanno <manuel.jehanno.etu@univ-lemans.fr>
2020-11-20 14:55:04 +0100

    Premiere belle cardview

--- app/app/src/main/java/com/example/starsnow/MainActivity.java
+++ app/app/src/main/java/com/example/starsnow/MainActivity.java
@@ -16,11 +16,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+/*
         liste_aeroports = (ListView) findViewById(R.id.liste_aeroports);
 
         final ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
                 android.R.layout.simple_list_item_1, aeroports);
-        liste_aeroports.setAdapter(adapter);
+        liste_aeroports.setAdapter(adapter); */
     }
 }
