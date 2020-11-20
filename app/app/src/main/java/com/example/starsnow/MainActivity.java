d4a1704da981a7171b46fac96856418cff4ae82d
Jehanno <manuel.jehanno.etu@univ-lemans.fr>
2020-11-20 16:44:17 +0100

    Tout bon sauf apparaitre ajout lors de suppression aeropp

--- app/app/src/main/java/com/example/starsnow/MainActivity.java
+++ app/app/src/main/java/com/example/starsnow/MainActivity.java
@@ -7,13 +7,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
 
     private CardView cardAerop1, cardAerop2, cardAerop3, cardAerop4;
     private Button ajoutAerop;
-    private int nbAerop = 0;
+    private ImageButton suppr1, suppr2, suppr3, suppr4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,40 +25,75 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         cardAerop2 = (CardView) findViewById(R.id.card_aerop2);
         cardAerop3 = (CardView) findViewById(R.id.card_aerop3);
         cardAerop4 = (CardView) findViewById(R.id.card_aerop4);
-
         cardAerop1.setVisibility(View.GONE);
         cardAerop2.setVisibility(View.GONE);
         cardAerop3.setVisibility(View.GONE);
         cardAerop4.setVisibility(View.GONE);
 
+        suppr1 = findViewById((R.id.icone_supp1));
+        suppr2 = findViewById((R.id.icone_supp2));
+        suppr3 = findViewById((R.id.icone_supp3));
+        suppr4 = findViewById((R.id.icone_supp4));
+        suppr1.setOnClickListener(handlerSuppr1);
+        suppr2.setOnClickListener(handlerSuppr2);
+        suppr3.setOnClickListener(handlerSuppr3);
+        suppr4.setOnClickListener(handlerSuppr4);
+
+
         ajoutAerop = (Button) findViewById(R.id.ajoutAerop);
-
-        ajoutAerop.setOnClickListener(this);
+        ajoutAerop.setOnClickListener(handlerAjout);
 
     }
 
-    @Override
-    public void onClick(View v) {
-        if(nbAerop == 0)
-        {
-            cardAerop1.setVisibility(View.VISIBLE);
-            nbAerop++;
+    View.OnClickListener handlerSuppr1 = new View.OnClickListener() {
+        public void onClick(View v) {
+            cardAerop1.setVisibility(View.GONE);
         }
-        else if (nbAerop == 1) {
-            cardAerop2.setVisibility(View.VISIBLE);
-            nbAerop++;
+    };
+    View.OnClickListener handlerSuppr2 = new View.OnClickListener() {
+        public void onClick(View v) {
+            cardAerop2.setVisibility(View.GONE);
         }
-        else if (nbAerop == 2) {
-            cardAerop3.setVisibility(View.VISIBLE);
-            nbAerop++;
+    };
+    View.OnClickListener handlerSuppr3 = new View.OnClickListener() {
+        public void onClick(View v) {
+            cardAerop3.setVisibility(View.GONE);
         }
-        else if (nbAerop == 3) {
-            cardAerop4.setVisibility(View.VISIBLE);
-            ajoutAerop.setVisibility(View.GONE);
-            nbAerop = 4;
+    };
+    View.OnClickListener handlerSuppr4 = new View.OnClickListener() {
+        public void onClick(View v) {
+            cardAerop4.setVisibility(View.GONE);
         }
-        else {
-            //affiche popup 
+    };
+
+    View.OnClickListener handlerAjout = new View.OnClickListener() {
+        public void onClick(View v) {
+            if(cardAerop1.getVisibility() == View.GONE){
+                cardAerop1.setVisibility(View.VISIBLE);
+            }
+            else {
+                if(cardAerop2.getVisibility() == View.GONE){
+                    cardAerop2.setVisibility(View.VISIBLE);
+                }
+                else {
+                    if(cardAerop3.getVisibility() == View.GONE){
+                        cardAerop3.setVisibility(View.VISIBLE);
+                    }
+                    else{
+                        if(cardAerop4.getVisibility() == View.GONE){
+                            cardAerop4.setVisibility(View.VISIBLE);
+                        }
+                        else{
+                            //affiche popup
+                        }
+                    }
+
+                }
+            }
+            if((cardAerop1.getVisibility() == View.VISIBLE) && (cardAerop2.getVisibility() == View.VISIBLE) && (cardAerop3.getVisibility() == View.VISIBLE) && (cardAerop4.getVisibility() == View.VISIBLE)){
+                ajoutAerop.setVisibility(View.GONE);
+            }
         }
-    }
+
+    };
 }
