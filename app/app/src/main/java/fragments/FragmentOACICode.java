99e86b9b4847bc522a3096ea5841295a1ed898f3
Tom <tom.pastor.etu@univ-lemans.fr>
2020-12-02 11:05:27 +0100

    Modification

--- app/app/src/main/java/fragments/FragmentOACICode.java
+++ app/app/src/main/java/fragments/FragmentOACICode.java
@@ -20,7 +20,7 @@ public class FragmentOACICode extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_oaci_code, container, false);
         TextView txt = (TextView) view.findViewById(R.id.section_label);
-        txt.setText("hello2");
+        txt.setText("Chargement du snowtam...");
         return view;
     }
 }
